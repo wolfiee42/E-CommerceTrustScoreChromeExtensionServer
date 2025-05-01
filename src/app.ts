089ff05faca74ffec495159c77d28c3f31e52e9c
wolfiee42 <saifalislam2022@gmail.com>
2025-05-01 11:35:30 +0600

    chore: repository setup.

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import notFound from './app/middlewares/notFound'
+import router from './app/routes'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+// middleware
+const app = express()
+app.use(express.json())
+app.use(cors({ origin: ['http://localhost:5173', '*'] }))
+app.use(cookieParser())
+
+// api endpoints
+app.use('/api/v1', router)
+
+// error handler
+app.use(globalErrorHandler)
+
+// 404 page
+app.use(notFound)
+
+export default app
