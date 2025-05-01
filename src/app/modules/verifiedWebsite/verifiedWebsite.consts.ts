ece8b10c8e3d5f4ba3fb4c3ac8e3ce73f21373b2
wolfiee42 <saifalislam2022@gmail.com>
2025-05-01 11:59:12 +0600

    feat: creating constant variable for website.

---
+++ src/app/modules/verifiedWebsite/verifiedWebsite.consts.ts
@@ -0,0 +1,20 @@
+export const defaultProductQuality = [
+  { name: 'High Quality', score: 1, baseRating: 1 },
+  { name: 'Poor Quality', score: 1, baseRating: 1 },
+  { name: 'premium Quality', score: 1, baseRating: 1 },
+  { name: 'Durable', score: 1, baseRating: 1 },
+]
+
+export const defaultCustomerServices = [
+  { name: 'Helpful Support', score: 1, baseRating: 1 },
+  { name: 'Rude Staff', score: 1, baseRating: 1 },
+  { name: 'Delayed Response', score: 1, baseRating: 1 },
+  { name: 'Professional Services', score: 1, baseRating: 1 },
+]
+
+export const defaultPlatformExperience = [
+  { name: 'Easy to use', score: 1, baseRating: 1 },
+  { name: 'Complicated navigation', score: 1, baseRating: 1 },
+  { name: 'Payment Issue', score: 1, baseRating: 1 },
+  { name: 'Data Privacy', score: 1, baseRating: 1 },
+]
