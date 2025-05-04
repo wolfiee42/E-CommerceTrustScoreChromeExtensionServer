7a4f38762d9bf65793110359463917e4bf451820
wolfiee42 <saifalislam2022@gmail.com>
2025-05-04 11:21:18 +0600

    fix: resolved the cors issue.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import notFound from './app/middlewares/notFound'
 import router from './app/routes'
@@ -8,7 +8,7 @@ import cors from 'cors'
 // middleware
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: ['http://localhost:5173', '*'] }))
+app.use(cors({ origin: '*' }))
 app.use(cookieParser())
 
 // api endpoints
